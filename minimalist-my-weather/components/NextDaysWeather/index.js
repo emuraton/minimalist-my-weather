4cc86596d22d935322ad56d9c6dabe6c30f37542
Emilien Muraton <emilien.muraton@gmail.com>
2019-05-13 15:35:49 +0100

    add NextDaysWeather

---
+++ minimalist-my-weather/components/NextDaysWeather/index.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import { StyleSheet, ScrollView, View, Text, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cell: {
+    flex: 1,
+    // justifyContent: 'space-evenly',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  image: {
+    height: 64,
+    width: 64,
+  },
+  temperature: {
+    padding: 9,
+    textAlign: 'center',
+  },
+});
+
+function NextDaysWeather() {
+  const weathers = [
+    {
+      id: 4673120805847040,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'SW',
+      created: '2019-04-25T15:06:17.064868Z',
+      applicable_date: '2019-04-25',
+      min_temp: 11.73,
+      max_temp: 14.56,
+      the_temp: 15.22,
+      wind_speed: 4.762288250462632,
+      wind_direction: 234.9872083642845,
+      air_pressure: 1013.9200000000001,
+      humidity: 76,
+      visibility: 11.534202755905511,
+      predictability: 70,
+    },
+    {
+      id: 6487367470809088,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'WSW',
+      created: '2019-04-25T15:06:20.059768Z',
+      applicable_date: '2019-04-26',
+      min_temp: 11.745000000000001,
+      max_temp: 20.045,
+      the_temp: 21.39,
+      wind_speed: 4.290698108133832,
+      wind_direction: 239.82798463916453,
+      air_pressure: 1014.625,
+      humidity: 65,
+      visibility: 13.85160661735465,
+      predictability: 70,
+    },
+    {
+      id: 5714445961527296,
+      weather_state_name: 'Heavy Cloud',
+      weather_state_abbr: 'hc',
+      wind_direction_compass: 'SW',
+      created: '2019-04-25T15:06:23.272691Z',
+      applicable_date: '2019-04-27',
+      min_temp: 12.18,
+      max_temp: 20.58,
+      the_temp: 21.265,
+      wind_speed: 4.474460324184098,
+      wind_direction: 227.0023469842594,
+      air_pressure: 1013.875,
+      humidity: 64,
+      visibility: 12.309984689413824,
+      predictability: 71,
+    },
+    {
+      id: 6156191132549120,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'SSW',
+      created: '2019-04-25T15:06:26.455455Z',
+      applicable_date: '2019-04-28',
+      min_temp: 11.55,
+      max_temp: 20.759999999999998,
+      the_temp: 19.665,
+      wind_speed: 5.900157922890322,
+      wind_direction: 210.5621338363528,
+      air_pressure: 1011.36,
+      humidity: 65,
+      visibility: 13.20320577825499,
+      predictability: 70,
+    },
+    {
+      id: 6159086678704128,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'SSW',
+      created: '2019-04-25T15:06:29.089577Z',
+      applicable_date: '2019-04-29',
+      min_temp: 11.045,
+      max_temp: 18.810000000000002,
+      the_temp: 17.745,
+      wind_speed: 5.687262867098809,
+      wind_direction: 213.50295800170935,
+      air_pressure: 1009.925,
+      humidity: 66,
+      visibility: 13.020211962141095,
+      predictability: 70,
+    },
+    {
+      id: 4679222343761920,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'SW',
+      created: '2019-04-25T15:06:32.094357Z',
+      applicable_date: '2019-04-30',
+      min_temp: 11.44,
+      max_temp: 17.035,
+      the_temp: 17.65,
+      wind_speed: 5.339391553328561,
+      wind_direction: 224.0,
+      air_pressure: 1010.43,
+      humidity: 67,
+      visibility: 9.997862483098704,
+      predictability: 70,
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        {weathers.map(
+          ({
+            weather_state_abbr: weatherAbbr,
+            min_temp: minTemp,
+            max_temp: maxTemp,
+            id,
+          }) => (
+            <View style={styles.cell} key={id}>
+              <Text style={styles.temperature}>
+                {`${Math.round(minTemp)}°  ${Math.round(maxTemp)}°`}
+              </Text>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: `https://www.metaweather.com/static/img/weather/png/64/${weatherAbbr}.png`,
+                }}
+              />
+            </View>
+          ),
+        )}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default NextDaysWeather;
--- minimalist-my-weather/components/WeatherView/index.js
+++ minimalist-my-weather/components/WeatherView/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { Image, StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
   image: {
-    height: 200,
-    width: 200,
+    height: 150,
+    width: 150,
   },
   temperature: {
-    fontSize: 48,
-    color: 'red',
+    fontSize: 80,
+    color: 'lightblue',
     paddingTop: 20,
+    paddingLeft: 30,
   },
 });
 
@@ -17,20 +22,23 @@ const BASE_URL = 'https://www.metaweather.com';
 
 export default function WeatherView({ weathers }) {
   if (!weathers) return null;
+
   const {
     weather_state_abbr: weatherAbbr,
     the_temp: temperature,
   } = weathers[0];
 
   return (
-    <View>
+    <View style={styles.container}>
       <Image
         source={{
           uri: `${BASE_URL}/static/img/weather/png/${weatherAbbr}.png`,
         }}
         style={styles.image}
       />
-      <Text style={styles.temperature}>{Math.round(temperature)}</Text>
+      <Text style={styles.temperature}>{Math.round(temperature)}
+°
+</Text>
     </View>
   );
 }
